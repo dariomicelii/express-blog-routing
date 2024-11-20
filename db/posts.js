bf8da9750c3802bd71a7b26cea62252a6ff66169
dariomicelii <dariomiceli01@gmail.com>
2024-11-20 15:49:32 +0100

    add db

---
+++ db/posts.js
@@ -0,0 +1,36 @@
+let posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Ricetta ciambellone",
+    immagine: "./images/ciambellone.jpg",
+    tag: ["#ciambellone", "#cucina", "#dolce"],
+  },
+
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "Ricetta cracker alla barbabietola",
+    immagine: "./images/cracker_barbabietola.jpg",
+    tag: ["#crackerbarbabietola", "#cucina", "#verdure"],
+  },
+
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Ricetta Pane fritto dolce",
+    immagine: "./images/pane_fritto_dolce.jpg",
+    tag: ["#Panefrittodolce", "#cucina", "#dolce"],
+  },
+
+  {
+    titolo: "Pasta alla barbabietola",
+    contenuto: "Ricetta Pasta alla barbabietola",
+    immagine: "./images/pasta_barbabietola.jpg",
+    tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
+  },
+
+  {
+    titolo: "Torta paesana",
+    contenuto: "Ricetta torta paesana",
+    immagine: "./images/torta_paesana.jpg",
+    tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
+  },
+];
