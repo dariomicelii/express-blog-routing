d5061201275b2b69c787a820246a1fcb779f795d
dariomicelii <dariomiceli01@gmail.com>
2024-11-20 17:21:08 +0100

    post.js (db)

--- db/posts.js
+++ db/posts.js
@@ -2,35 +2,35 @@ let posts = [
   {
     titolo: "Ciambellone",
     contenuto: "Ricetta ciambellone",
-    immagine: "./images/ciambellone.jpg",
+    immagine: "/images/ciambellone.jpg",
     tag: ["#ciambellone", "#cucina", "#dolce"],
   },
 
   {
     titolo: "Cracker alla barbabietola",
     contenuto: "Ricetta cracker alla barbabietola",
-    immagine: "./images/cracker_barbabietola.jpg",
+    immagine: "/images/cracker_barbabietola.jpg",
     tag: ["#crackerbarbabietola", "#cucina", "#verdure"],
   },
 
   {
     titolo: "Pane fritto dolce",
     contenuto: "Ricetta Pane fritto dolce",
-    immagine: "./images/pane_fritto_dolce.jpg",
+    immagine: "/images/pane_fritto_dolce.jpg",
     tag: ["#Panefrittodolce", "#cucina", "#dolce"],
   },
 
   {
     titolo: "Pasta alla barbabietola",
     contenuto: "Ricetta Pasta alla barbabietola",
-    immagine: "./images/pasta_barbabietola.jpg",
+    immagine: "/images/pasta_barbabietola.jpg",
     tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
   },
 
   {
     titolo: "Torta paesana",
     contenuto: "Ricetta torta paesana",
-    immagine: "./images/torta_paesana.jpg",
+    immagine: "/images/torta_paesana.jpg",
     tag: ["#Pastaallabarbabietola", "#cucina", "#pasta", "#verdure"],
   },
 ];
