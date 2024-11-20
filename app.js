96d65c7ffea820f06d2b5014ec3704cae6226206
dariomicelii <dariomiceli01@gmail.com>
2024-11-20 16:22:42 +0100

    app.js attuale

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 app.use(express.static("public"));
 
 const postsRouter = require("./routers/posts.js");
 
-app.get("/", (req, res) => {
-  res.send("Server del mio blog");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server del mio blog");
+// });
 
-app.use("/", postsRouter);
+// app.use("/", postsRouter);
 app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
 
-let postNum = posts.length;
+// let postNum = posts.length;
 
 // app.get("/bacheca", (req, res) => {
 //   let resoconto = {
